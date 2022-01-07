2a294321974beb234cf4dacf9eabc693b0a23fea
Lucas Bertin <lucas.ce.bertin@gmail.com>
2022-01-07 17:53:25 -0300

    refactor: now the server is  still alive

--- src/nopsteam/messaging.clj
+++ src/nopsteam/messaging.clj
@@ -1,9 +1,17 @@
 (ns nopsteam.messaging
-  (:require [byte-streams :as bs])
+  (:require [byte-streams :as bs]
+            [taoensso.timbre :as t])
   (:import [java.net DatagramPacket]))
 
 (defn empty-message [n]
   (new DatagramPacket (byte-array n) n))
+
+;; TODO: testar essa fita
+(defn receive [message-size socket]
+  (let [received-packet-hole (empty-message message-size)]
+    (.receive socket received-packet-hole)
+    (t/log :info {:packet-data (bs/to-string (.getData received-packet-hole))})
+    received-packet-hole))
 
 (defn new-packet
   ([message socket]
--- src/nopsteam/server.clj
+++ src/nopsteam/server.clj
@@ -1,37 +1,29 @@
 (ns nopsteam.server
-  (:require [byte-streams :as bs]
-            [nopsteam.messaging :as m])
-  (:import [java.net InetAddress InetSocketAddress DatagramSocket]))
+  (:require [nopsteam.adapters :as a]
+            [nopsteam.logics :as l]
+            [nopsteam.messaging :as m]
+            [taoensso.timbre :as t])
+  (:import [java.net DatagramSocket]))
+
+(def active-requests (atom []))
 
 (def udp-port 7070)
 
-(defn future-receive [server client-id]
-  (let [packet (m/empty-message 1024)]
-    (println "Waiting for client:" client-id)
-    (.receive server packet)
-    (println "Received packet:" (bs/to-string (.getData packet)))
-    packet))
+(defn connect-peers [udp-server [client-1 client-2]]
+  (.send udp-server (m/new-packet (a/client-info->str client-2) (:socket client-1)))
+  (.send udp-server (m/new-packet (a/client-info->str client-1) (:socket client-2))))
 
 (defn listen
   [_]
-  (let [udp-server (DatagramSocket. udp-port)
+  (let [udp-server (DatagramSocket. udp-port)]
+    (t/log :info "server started!")
+    (loop []
+      (let [client-info (-> (m/receive 1024 udp-server) a/request->client-info)]
+        (when (l/request-not-exists? client-info @active-requests)
+          (swap! active-requests conj client-info))
 
-        client-packet-1 (future (future-receive udp-server 1))
-        client-packet-2 (future (future-receive udp-server 2))
+        (when (l/has-peers? @active-requests)
+          (connect-peers udp-server @active-requests)
+          (reset! active-requests [])))
+      (recur))))
 
-        client-socket-1 (-> @client-packet-1 ^InetSocketAddress (.getSocketAddress))
-        _debug-socket-1 (println "socket-address 1:" (.toString client-socket-1))
-        client-address-1 (-> @client-packet-1 ^InetAddress (.getAddress))
-        client-port-1 (-> @client-packet-1 ^int (.getPort))
-        client-info-1 (str (.getHostAddress client-address-1) "-" client-port-1 "-")
-
-        client-socket-2 (-> @client-packet-2 ^InetSocketAddress (.getSocketAddress))
-        _debug-socket-2 (println "socket-address 2:" (.toString client-socket-2))
-        client-address-2 (-> @client-packet-2 ^InetAddress (.getAddress))
-        client-port-2 (-> @client-packet-2 ^int (.getPort))
-        client-info-2 (str (.getHostAddress client-address-2) "-" client-port-2 "-")]
-
-    (.send udp-server (m/new-packet client-info-2 client-socket-1))
-    (.send udp-server (m/new-packet client-info-1 client-socket-2))
-
-    (.close udp-server)))
