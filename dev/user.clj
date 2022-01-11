2f3f2705736eb54cf4e5de2884c9d0f6a65e5850
Rafael Delboni <rafadelboni@gmail.com>
2022-01-11 18:38:12 -0300

    chore: Requive base project for malli

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,7 @@
 (ns user
   (:require [malli.dev :as dev]
-            [malli.dev.pretty :as pretty]))
+            [malli.dev.pretty :as pretty]
+            [nopsteam.udpunchat]))
 
 (defn start
   []
